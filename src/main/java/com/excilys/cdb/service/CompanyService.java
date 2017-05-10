a65d686e23ca3cb5ecd436cd2343d5b56b0ecad5
Gaetan Puget <gpuget@excilys.com>
2017-05-10 16:28:45 +0200

    [FIX] CompanyService delete autoCommit false and rollback

--- src/main/java/com/excilys/cdb/service/CompanyService.java
+++ src/main/java/com/excilys/cdb/service/CompanyService.java
@@ -32,15 +32,23 @@ public enum CompanyService {
     	ThreadLocal<Connection> sharedConnectionThread = new ThreadLocal<>();
     	
     	try (Connection connection = Connector.INSTANCE.getConnection()) {
+    		connection.setAutoCommit(false);
     		sharedConnectionThread.set(connection);
-    		ComputerDAOImpl.INSTANCE.deleteFromCompany(id, sharedConnectionThread.get());
-    		CompanyDAOImpl.INSTANCE.delete(id, sharedConnectionThread.get());
+    		try {
+				ComputerDAOImpl.INSTANCE.deleteFromCompany(id, sharedConnectionThread.get());
+				CompanyDAOImpl.INSTANCE.delete(id, sharedConnectionThread.get());
+			} catch (Exception e) {
+				connection.rollback();
+				throw e;
+			}
+			connection.commit();
     	} catch (Exception e) {
         	String message = "Error : Service has not been able to correctly delete the company and its computers.";
         	LOGGER.error(message);
             throw new RuntimeException(message, e);
     	} finally {
     		sharedConnectionThread.remove();
+    		System.gc();
     	}
     }
 }
