3fc62bc06935a4b7f59926eaac00f9b9ea930ee5
Gaetan Puget <gpuget@excilys.com>
2017-05-16 11:26:26 +0200

    [FEATURE] findAll(int,int), create, delete, delete(List) with JDBCTemplate

--- src/main/java/com/excilys/cdb/persistence/impl/ComputerDAOImpl.java
+++ src/main/java/com/excilys/cdb/persistence/impl/ComputerDAOImpl.java
@@ -4,14 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.cdb.exception.DAOException;
@@ -26,11 +33,11 @@ public class ComputerDAOImpl implements ComputerDAO {
     private static final Logger LOGGER = LoggerFactory.getLogger(ComputerDAOImpl.class);
 
     private static final String FIND_QUERY = "SELECT cpu.id, cpu.name, cpu.introduced, cpu.discontinued, cpu.company_id, "
-                                            + "com.name AS company_name " + "FROM computer AS  cpu "
-                                            + "LEFT JOIN company AS com ON cpu.company_id = com.id WHERE cpu.id = ?";
+            + "com.name AS company_name " + "FROM computer AS  cpu "
+            + "LEFT JOIN company AS com ON cpu.company_id = com.id WHERE cpu.id = ?";
     private static final String FIND_ALL = "SELECT cpu.id, cpu.name, cpu.introduced, cpu.discontinued, cpu.company_id, "
-                                            + "com.name AS company_name " + "FROM computer AS  cpu "
-                                            + "LEFT JOIN company as com ON cpu.company_id = com.id";
+            + "com.name AS company_name " + "FROM computer AS  cpu "
+            + "LEFT JOIN company as com ON cpu.company_id = com.id";
     private static final String CREATE_QUERY = "INSERT INTO computer (name, introduced, discontinued, company_id) VALUES (?, ?, ?, ?)";
     private static final String DELETE_QUERY = "DELETE FROM computer WHERE computer.id = ?";
     private static final String UPDATE_QUERY = "UPDATE computer SET name = ?, introduced = ?, discontinued = ?, company_id = ? WHERE id = ?";
@@ -42,30 +49,38 @@ public class ComputerDAOImpl implements ComputerDAO {
 
     @Autowired
     private Connector connector;
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void init() {
+        LOGGER.info("Initialization ComputerDAO...");
+        LOGGER.debug("Initialization JdbcTemplate");
+        jdbcTemplate = new JdbcTemplate(connector.getDataSource());
+    }
 
     @Override
     public Computer create(Computer computer) {
         LOGGER.info("Create computer : " + computer);
-        String message = "Error : DAO has not been able to correctly create the entity.";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        try (Connection connection = connector.getConnection();
-                PreparedStatement statement = connection.prepareStatement(CREATE_QUERY,
-                        Statement.RETURN_GENERATED_KEYS);) {
-            setStatementValues(statement, computer);
-            LOGGER.debug("Query : " + statement);
-            statement.executeUpdate();
-            try (ResultSet rs = statement.getGeneratedKeys()) {
-                if (rs.first()) {
-                    long id = rs.getLong(1);
-                    computer.setId(id);
-                    LOGGER.debug("Generated id : " + id);
-                } else {
-                    LOGGER.error(message);
-                    throw new DAOException(message);
+        try {
+            jdbcTemplate.update(new PreparedStatementCreator() {
+                @Override
+                public PreparedStatement createPreparedStatement(Connection con)
+                        throws SQLException {
+                    PreparedStatement statement = con.prepareStatement(CREATE_QUERY);
+                    setStatementValues(statement, computer);
+                    LOGGER.debug("Query : " + statement);
+
+                    return statement;
                 }
-            }
+            }, keyHolder);
 
-        } catch (SQLException e) {
+            long id = (long) keyHolder.getKey();
+            LOGGER.debug("Generated id : " + id);
+            computer.setId(id);
+        } catch (DataAccessException e) {
+            String message = "Error : DAO has not been able to correctly create the entity.";
             LOGGER.error(message);
             throw new DAOException(message, e);
         }
@@ -76,12 +91,11 @@ public class ComputerDAOImpl implements ComputerDAO {
     @Override
     public void delete(long id) {
         LOGGER.info("Delete computer by id : " + id);
-        try (Connection connection = connector.getConnection();
-                PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)) {
-            statement.setLong(1, id);
-            LOGGER.debug("Query : " + statement);
-            statement.executeUpdate();
-        } catch (SQLException e) {
+
+        try {
+            LOGGER.debug("Query : " + DELETE_QUERY);
+            jdbcTemplate.update(DELETE_QUERY, id);
+        } catch (DataAccessException e) {
             String message = "Error : DAO has not been able to correctly delete the entity.";
             LOGGER.error(message);
             throw new DAOException(message, e);
@@ -91,19 +105,19 @@ public class ComputerDAOImpl implements ComputerDAO {
     @Override
     public void delete(List<Long> idsList) {
         LOGGER.info("Delete all computer in : " + idsList);
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sqlQueryBuilder = new StringBuilder(DELETE_IN);
 
-        sb.append(idsList.get(0));
+        sqlQueryBuilder.append('(');
+        sqlQueryBuilder.append(idsList.get(0));
         for (int i = 1; i < idsList.size(); i++) {
-            sb.append(", ").append(idsList.get(i));
+            sqlQueryBuilder.append(", ").append(idsList.get(i));
         }
+        sqlQueryBuilder.append(')');
 
-        try (Connection connection = connector.getConnection();
-                PreparedStatement statement = connection
-                        .prepareStatement(DELETE_IN + '(' + sb.toString() + ')')) {
-            LOGGER.debug("Query : " + statement);
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        LOGGER.debug("Query : " + sqlQueryBuilder.toString());
+        try {
+            jdbcTemplate.update(sqlQueryBuilder.toString());
+        } catch (DataAccessException e) {
             String message = "Error : DAO has not been able to correctly delete entities.";
             LOGGER.error(message);
             throw new DAOException(message, e);
@@ -150,27 +164,17 @@ public class ComputerDAOImpl implements ComputerDAO {
     @Override
     public List<Computer> findAll(int limit, int offset) {
         LOGGER.info("Find all computers : " + limit + " " + offset);
-        ArrayList<Computer> computers = new ArrayList<Computer>();
 
-        try (Connection connection = connector.getConnection();
-                PreparedStatement statement = connection
-                        .prepareStatement(FIND_ALL + BOUNDED_RESULT)) {
-            statement.setInt(1, limit);
-            statement.setInt(2, offset);
-            LOGGER.debug("Query : " + statement);
-            try (ResultSet resultSet = statement.executeQuery();) {
-                while (resultSet.next()) {
-                    computers.add(ComputerRowMapper.mapComputer(resultSet));
-                }
-            }
-            LOGGER.debug("Found computers : " + computers);
-        } catch (SQLException e) {
+        try {
+            String sqlQuery = FIND_ALL + BOUNDED_RESULT;
+            LOGGER.debug("Query : " + sqlQuery);
+            return jdbcTemplate.query(sqlQuery, new Object[] { limit, offset },
+                    new int[] { Types.INTEGER, Types.INTEGER }, new ComputerRowMapper());
+        } catch (DataAccessException e) {
             String message = "Error : DAO has not been able to correctly find all entities.";
             LOGGER.error(message);
             throw new DAOException(message, e);
         }
-
-        return computers;
     }
 
     @Override
@@ -272,7 +276,7 @@ public class ComputerDAOImpl implements ComputerDAO {
      * @param computer computer with values to load in statement
      * @throws SQLException SQL exception
      */
-    private void setStatementValues(PreparedStatement ps, Computer computer) throws SQLException {
+    public void setStatementValues(PreparedStatement ps, Computer computer) throws SQLException {
         LOGGER.info("Set values in statement");
         Company company = computer.getManufacturer();
 
