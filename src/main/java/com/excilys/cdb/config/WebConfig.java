0a365536c30bb6f082a7553db4e8e2e8f0713ad9
Gaetan Puget <gpuget@excilys.com>
2017-05-17 13:36:56 +0200

    [FIX] Add resource handler for css, js and fonts

--- src/main/java/com/excilys/cdb/config/WebConfig.java
+++ src/main/java/com/excilys/cdb/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -16,6 +17,12 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = "com.excilys.cdb.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        LOGGER.info("addResourceHandlers");
+        registry.addResourceHandler("/resources/**");
+    }
 
     @Bean
     public ViewResolver internalRessourceViewResolver() {
