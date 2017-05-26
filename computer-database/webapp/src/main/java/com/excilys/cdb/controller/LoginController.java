6dcf73fdb4c7e36ae5d94c4d539dfed2134add25
Gaetan Puget <gpuget@excilys.com>
2017-05-26 11:17:57 +0200

    [FEATURE] Spring security logout

--- computer-database/webapp/src/main/java/com/excilys/cdb/controller/LoginController.java
+++ computer-database/webapp/src/main/java/com/excilys/cdb/controller/LoginController.java
@@ -1,22 +1,49 @@
 package com.excilys.cdb.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
     public static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
-    
 
     @GetMapping(value = "/login")
-    public String get() {
-        return "login";
+    public ModelAndView login(  @RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout) {
+        ModelAndView model = new ModelAndView();
+
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("logout", "You have been logged out.");
+        }
+
+        return model;
     }
-    
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest req, HttpServletResponse resp) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(req, resp, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @GetMapping(value = "/errors/403")
     public String acessDenied() {
--- computer-database/webapp/src/main/webapp/login.jsp
+++ computer-database/webapp/src/main/webapp/login.jsp
@@ -14,29 +14,28 @@
 		<link href="resources/css/main.css" type="text/css" rel="stylesheet" media="screen">
 	</head>
 	<body>
-		<form name="f" action="login" method="POST">
-			<c:if test="${param.error != null}">        
-				<p>
-					Invalid username and password.
-				</p>
-			</c:if>
+		<div class = "container">
 			<c:if test="${param.logout != null}">
-				<p>
-					You have been logged out.
-				</p>
+				<div class="alert alert-success">
+				  <strong>Success :</strong> ${logout}
+				</div>
+			</c:if>			
+			
+			<c:if test="${param.error != null}">
+				<div class="alert alert-danger">
+				  <strong>Error :</strong> ${error}
+				</div>
 			</c:if>
-			<p>
-				<label for="username">Username</label>
-				<input type="text" id="username" name="username"/>
-			</p>
-			<p>
-				<label for="password">Password</label>
-				<input type="password" id="password" name="password"/>
-			</p>
-			<input type="hidden"      
-				name="${_csrf.parameterName}"
-				value="${_csrf.token}"/>
-			<button type="submit" class="btn">Log in</button>
-		</form>
+		
+			<form action="login" method="post">
+				<h2 class="form-signin-heading">Computer database</h2>
+				<br>
+				<input type="text" class="form-control" id="username" name="username" placeholder="Username" />
+				<input type="password" class="form-control" id="password" name="password" placeholder="Password" />
+					
+				<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
+				<button class="btn btn-lg btn-primary btn-block" name="Submit" value="Login" type="Submit">Login</button>
+			</form>
+		</div>
 	</body>
 </html>
